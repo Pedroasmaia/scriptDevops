582edc870f729d577807dee579b7a799bd7b4c3d
Pedro Maia <pedroasmaia@outlook.com>
2023-09-29 16:29:04 -0300

    Refazendo o Script para deletar wiki

---
+++ DevOps API/delete_wiki_repo.py
@@ -0,0 +1,52 @@
+import requests as r
+import base64
+
+
+def delete_wiki_repo(tokenPersonal,organization,project):
+    '''Deleta o repositório que a Wiki Utiliza
+    parameters:
+        tokenPersonal(string): Token Pessoal para autenticar a requisição, pode ser obtida através  do portal do Azure DevOps
+        project(string): Nome do Projeto
+        organization(string): Nome da Organização
+    '''
+    authorization = str(base64.b64encode(bytes(':'+tokenPersonal, 'ascii')), 'ascii')
+    headers = {
+    'Accept': 'application/json',
+    'Authorization': 'Basic '+authorization
+    }
+    wiki_url = f"https://dev.azure.com/{organization}/{project}/_apis/wiki/wikis"
+    request_response = r.get(url=wiki_url,headers=headers)
+    request_response = request_response.json()
+    if request_response['count'] == 1:
+        value_response = request_response['value']
+        value_response = value_response[0]
+        repository_id = value_response['repositoryId']
+        repo_url = f"https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repository_id}?api-version=7.2-preview.1"
+        repository_name = value_response['name']
+        confirm = input(f'Deseja apagar o {repository_name}? (y) Sim (n) Não: ')
+        if confirm.lower() == 'y':
+            request_delete = r.delete(url=repo_url,headers=headers)
+            if(str(request_delete.status_code)=='204'):
+                print('-------Apagado--------')
+                print(f'Nome: {repository_name}')
+                print(f'ID: {repository_id}')
+                print('----------------------')
+    else:
+        for repository in request_response['value']:
+            repository_name = repository['name']
+            repository_id = repository['repositoryId']
+            repo_url = f"https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repository_id}?api-version=7.2-preview.1"
+            print('---------------')
+            print(f'Nome: {repository_name}')
+            print(f'ID: {repository_id}')
+            print('---------------')
+            confirm = input(f'Deseja apagar o {repository_name}? (y) Sim (n) Não: ')
+            if confirm.lower() == 'y':
+                request_delete = r.delete(url=repo_url,headers=headers)
+                if(str(request_delete.status_code)=='204'):
+                    print('-------Apagado--------')
+                    print(f'Nome: {repository_name}')
+                    print(f'ID: {repository_id}')
+                    print('----------------------')
+                else:
+                    print(str(request_delete.status_code))
